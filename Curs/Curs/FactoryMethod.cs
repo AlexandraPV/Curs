9ecc901fb6c26a94a107c9e5b03bb3dda3072c3d
AlexandraPV <your_email@your-domain.com>
2017-04-26 02:26:12 +0300

    25.04

---
+++ Curs/Curs/FactoryMethod.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Curs
+
+{
+	interface IBook
+	{
+		string ShipFrom();
+	}
+
+	class BookA : IBook
+	{
+		public String ShipFrom()
+		{
+			return "PublishingHouse1";
+		}
+	}
+
+	class BookB : IBook
+	{
+		public String ShipFrom()
+		{
+			return "PublishingHouse2";
+		}
+	}
+
+	class DefaultProduct : IBook
+	{
+		public String ShipFrom()
+		{
+			return "Can`t publishing";
+		}
+	}
+
+	class Creator
+
+	{
+
+		public IBook FactoryMethod(string cover, string pages )         
+		{
+
+			if ((cover.Equals("soft")==true) && (pages.Equals("medium")==true))
+
+				return new BookA();
+
+			else
+
+			if ((cover.Equals("hard") == true) && (pages.Equals("good") == true))
+
+				return new BookB();
+
+			else return new DefaultProduct();
+		}
+	}
+
+
+
+class Program
+
+	{
+
+		static void Main()
+		{
+			Creator c = new Creator();
+			IBook book;
+
+				book = c.FactoryMethod("soft", "medium");
+				Console.WriteLine("New Book " + book.ShipFrom());
+
+
+			Console.ReadKey();
+
+		}
+
+	}
+
+}
+
---
+++ Curs/Curs/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace Curs
+{
+	/*class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+*/
+
+
+	public  class Person
+
+	{
+
+		public String name;
+		public String surname;
+		public int id;
+		public DateTime dateBirth;
+		List<BookAll> listBook = new List<BookAll>();
+
+		public Object Clone()
+		{
+			Object clone = null;
+			try
+			{
+				clone = this.MemberwiseClone();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			return clone;
+		}
+
+		public int getId()
+		{
+			return id;
+		}
+
+
+		public String getSurname()
+		{
+			return this.surname;
+		}
+
+		public String getName()
+		{
+			return this.name;
+		}
+
+		public DateTime getDateBirth()
+		{
+			return this.dateBirth;
+		}
+
+		public List<BookAll> getListBook()
+		{
+			return listBook;
+		}
+
+		public void Addtolist(BookAll book)
+		{
+			listBook.Add(book);
+		}
+
+		public void Removelist(BookAll book)
+		{
+			listBook.Remove(book);
+		}
+
+	}
+
+	public abstract class BookAll
+
+	{
+		public int id;
+		public String name;
+		public String autor;
+		public String ganre;
+		public String type;
+		public int state;
+		public bool personState;
+
+
+		public Object Clone()
+		{
+			Object clone = null;
+			try
+			{
+				clone = this.MemberwiseClone();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			return clone;
+		}
+
+		public String getAutor()
+		{
+			return autor;
+		}
+
+
+		public String getGanre()
+		{
+			return ganre;
+		}
+
+		public String getType()
+		{
+			return this.type;
+		}
+
+		public String getName()
+		{
+			return name;
+		}
+
+		public int getState()
+		{
+			return state;
+		}
+
+		public int getId()
+		{
+			return id;
+		}
+
+		public bool getStatePerson()
+		{
+			return personState;
+		}
+
+		public void setState(int stateNew)
+		{
+			state = stateNew;
+		}
+
+		public void setStatePerson(bool stateNew)
+		{
+			personState = stateNew;
+		}
+
+
+	}
+
+}
